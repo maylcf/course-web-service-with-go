b9ac6ed2e08098e6177489e29f9bbe0033d68107
maylcf <mayaralcabral@gmail.com>
2021-02-21 17:19:09 -0500

    Implement product handler.

--- Pluralsight/Creating-Web-Services-Go/webservice/main.go
+++ Pluralsight/Creating-Web-Services-Go/webservice/main.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type fooHandler struct {
@@ -72,6 +74,77 @@ func getNextID() int {
 	return highestID + 1
 }
 
+func findProductById(productID int) (*Product, int) {
+	for i, product := range productList {
+		if product.ProductID == productID {
+			return &product, i
+		}
+	}
+	return nil, 0
+}
+
+func productHandler(w http.ResponseWriter, r *http.Request) {
+	urlPathSegments := strings.Split(r.URL.Path, "products/")
+
+	productID, err := strconv.Atoi(urlPathSegments[len(urlPathSegments)-1])
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	product, listItemIndex := findProductById(productID)
+	if product == nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	switch r.Method {
+	case http.MethodGet:
+		// Return a single product
+		productJSON, err := json.Marshal(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(productJSON)
+
+	case http.MethodPost:
+		// Update product in list
+		var updatedProduct Product
+
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		err = json.Unmarshal(bodyBytes, &updatedProduct)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		// Verify product ID
+		if updatedProduct.ProductID != productID {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		// Update product
+		product = &updatedProduct
+		productList[listItemIndex] = *product
+
+		// Return success
+		w.WriteHeader(http.StatusOK)
+		return
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+}
+
 func productsHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
@@ -123,6 +196,7 @@ func main() {
 	// http.HandleFunc("/bar", barHandler)
 
 	http.HandleFunc("/products", productsHandler)
+	http.HandleFunc("/products/", productHandler)
 
 	http.ListenAndServe(":5000", nil)
 }
