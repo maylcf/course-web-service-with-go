02fe5780a10ac4a27dd2635d9ed75c9578490d45
maylcf <mayaralcabral@gmail.com>
2021-02-21 22:40:33 -0500

    Create middleware.go file.

---
+++ Pluralsight/Creating-Web-Services-Go/webservice/cors/middleware.go
@@ -0,0 +1,18 @@
+package cors
+
+import (
+	"net/http"
+)
+
+// Middleware
+
+func Middleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Content-Type", "application/json")
+		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Add("Access-Control-Allow-Methods", "Accept, Content-Type, Content-Length")
+
+		handler.ServeHTTP(w, r)
+	})
+}
