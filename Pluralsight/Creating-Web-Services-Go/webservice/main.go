aca7240f432bf7a8b27fc0b35c9861adb311eeb7
Mayara Felix <mayaralcabral@gmail.com>
2021-02-22 23:31:18 -0500

    setup database.

--- Pluralsight/Creating-Web-Services-Go/webservice/main.go
+++ Pluralsight/Creating-Web-Services-Go/webservice/main.go
@@ -4,11 +4,14 @@ import (
 	"net/http"
 
 	"github.com/maylcf/learning-go/product"
+	"github.com/maylcf/learning-go/database"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 const apiBasePath = "/api"
 
 func main() {
+    database.SetupDatabase()
 	product.SetupRoutes(apiBasePath)
 	http.ListenAndServe(":5000", nil)
 }
